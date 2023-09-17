6ae086656d6517f7ca83892e846f8c2de1575e7c
Halil <113803550+RezoSenpai@users.noreply.github.com>
2023-09-17 11:36:21 +0200

    Create secret.py
    Example secret file,

---
+++ secret.py
@@ -0,0 +1,5 @@
+# Diablo 2 TerrorZone tracker
+tokenz = "[YOUR API KEY HERE]"
+header_email = "[YOUR EMAIL]"
+header_platform = "Discord"
+header_repo = "https://github.com/RezoSenpai/discord-d2-terrorzone"
